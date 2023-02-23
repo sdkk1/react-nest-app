f4987678ee396984a227e79aa5812af2f6d8effc
Keita <fast.break.baske03@gmail.com>
2023-02-23 19:06:58 +0900

    add: Mainページを作成

--- frontend/src/components/Main.tsx
+++ frontend/src/components/Main.tsx
@@ -1,10 +1,36 @@
+import { useQuery } from "@apollo/client";
+import { Stack, Typography } from "@mui/material";
+import jwtDecode from "jwt-decode";
+import { GET_TASKS } from "../queries/taskQueries";
+import { Payload } from "../types/payload";
+import { Task } from "../types/task";
+import AddTask from "./AddTask";
 import Header from "./Header";
+import Loading from "./Loading";
+import TaskTable from "./TaskTable";
 
 const Main = () => {
+  const token = localStorage.getItem("token") ?? "";
+  const decodedToken = jwtDecode<Payload>(token);
+  const userId = decodedToken.sub;
+
+  const { data, loading, error } = useQuery<{ getTasks: Task[] }>(GET_TASKS, {
+    variables: { userId },
+  });
+
   return (
     <>
       <Header />
-      <div>Main</div>
+      <Stack spacing={4} direction="column" m={8} alignItems="center">
+        {loading && <Loading />}
+        {error && <Typography color="red">エラーが発生しました</Typography>}
+        {!loading && !error && (
+          <>
+            <AddTask userId={userId} />
+            <TaskTable tasks={data?.getTasks} userId={userId} />
+          </>
+        )}
+      </Stack>
     </>
   );
 };
